7c5d49634cb9e6cf455e4aaeba454c67c4fe37cd
cvmelgarejo <101598815+cvmelgarejo@users.noreply.github.com>
2022-08-24 00:26:14 -0400

    mapa

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 import folium
 from folium import IFrame
 from folium.plugins import MarkerCluster
-import pandas as pd
+# import pandas as pd
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/db.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -50,12 +50,12 @@ class Emprendimientos(db.Model):
 
 @app.route('/registro', methods=['GET','POST'])
 def registro():
-    print("DFKJAJSBFDHBAHDFHJABHFDBAFDKAJFSDHVAKF")
     if request.method == 'POST':
-        print(request.form)
-        nombre_emp = request.form['nombre']
+        # print(request.form['nombre_emp'])
+        # nombre_emp = request.form['nombre_emp']
+        nombre_emp = "pepito"
         descripcion = request.form['about']
-        nombre = request.form['nombre/s']
+        nombre = request.form['nombre']
         apellido = request.form['apellido']
         contacto = request.form['numero_de_telefono']
         direccion = request.form['direccion']
@@ -73,11 +73,11 @@ def registro():
     return render_template('registro.html')
     
     #luego creamos una ruta para enlistar los emprendimientos
-@app.route ('/emprendimientos')
-def emprendimientos():
-	emprendimientos = Emprendimientos.query.all() ## de la tabla emprendimeintos, Hace una consulta de todos los registros en la base de datos y guarda en un objeto 
-	return render_template('emprendimientos.html', emprendimientos=emprendimientos) ##hacemos un render de una pagina HTML y a esa pagina le vamos a agarrar al objeto de 
-   ## de Python que tenemos, le vamos a guardar en el front end
+# @app.route ('/emprendimientos')
+# def emprendimientos():
+# 	emprendimientos = emprendimientos.query.all() ## de la tabla emprendimientos, Hace una consulta de todos los registros en la base de datos y guarda en un objeto 
+# 	return render_template('emprendimientos.html', emprendimientos=emprendimientos) ##hacemos un render de una pagina HTML y a esa pagina le vamos a agarrar al objeto de 
+#    ## de Python que tenemos, le vamos a guardar en el front end
 
 
 ##Creamos una ruta para borrar
@@ -91,29 +91,60 @@ def emprendimientos():
 
 
 
-@app.route('/mapa')
+@app.route('/map')
 def mapa():
     #Inicializamos el mapa 
-    map= folium.Map(
-        location=[-25.360106678758992, -57.63123446013285],
-        zoom_start=13,
-        )
-    # cluster = MarkerCluster().add_to(map)
-# mapa de datos
-    return map._repr_html_()
+#     map= folium.Map(
+#         location=[-25.360106678758992, -57.63123446013285],
+#         zoom_start=13,
+#         )
+#     # cluster = MarkerCluster().add_to(map)
+# # mapa de datos
+#     return map._repr_html_()
+
+#variables de coordenadas
+    coor_mapa = [-25.301379182412745, -57.58094636564712]
+    # coor_1 = [-25.301379182412745, -57.58094636564712]
+
+
+    #creación del mapa
+    mapa = folium.Map(location=coor_mapa,zoom_start=12)
+
+    # emprenIcon=folium.features.CustomIcon("static/mujerico.png",icon_size=(50,50))
+
+    emprendedoras = Emprendimientos.query.all()
+    for emprendedora in emprendedoras:
+        folium.Marker(location=[emprendedora.latitud, emprendedora.longitud],
+        popup=f'''
+                <h2>{emprendedora.nombre_emp}</h2>
+                <p>{emprendedora.descripcion}</p>
+                <p>celular:{emprendedora.contacto}</p>
+
+
+            ''').add_to(mapa)
+
+  #se puede usar despues de la ,Icom y dentro del html se puede hacer de todo,trabaja con "boostran"
+    #guardamos el mapa en un archivo html
+    mapa.save('templates/map.html')
+
+    return render_template('map.html')
 
 @app.route("/crear_alianza")
-def alianza():
+def crear_alianza():
     return render_template ("crear_alianza.html")
 
+
 @app.route("/alianzas")
-def alianza():
+def alianzas():
     return render_template ("Alianzas.html")
 
 @app.route("/vista_mapa")
-def alianza():
+def vista_mapa():
     return render_template ("vista_mapa.html")
 
+@app.route("/solicitud")
+def solicitud():
+    return render_template ("solicitud_Alian.html")
 
 
 if __name__ =='__main__':
